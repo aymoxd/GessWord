55d99bbdcf49f92d6660c9426fe9a4ccd3c9c9eb
aymane-3azi <carlejohnsonbus@gmail.com>
2025-09-23 23:18:31 +0100

    add popup

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 import { words } from "./data.js";
 const inputs = document.getElementById('inputs');
 const btn = document.getElementById('btn');
+const popup = document.getElementById('popup');
 
 
 let randomWord = words[Math.floor(Math.random() * words.length)];
@@ -36,11 +37,6 @@ function setAutoFocus(){
     input.addEventListener("input",()=>{
       const nextInput = allInputs[index + 1];
       if(nextInput && input.value !== "" ){
-
-      //  if(nextInput === allInputs[randomWord.length]){
-      //    checkTry();
-       // }
-
         nextInput.focus();
       }
     });
@@ -62,13 +58,17 @@ function setAutoFocus(){
 }
 generateArea();
 
-
 function checkTry(rowIndex){
       const divTry = document.querySelectorAll(`#inputBox${rowIndex} input`);
+      let userWord = "";
+
       for (let j = 0; j < randomWord.length; j++) {
        const input = divTry[j];
        const letter = input.value.toUpperCase();
        const correctLetter = randomWord[j].toUpperCase();
+         
+         userWord += letter;
+
 
        if(letter === correctLetter){
          input.style.background = "#008236"; //green
@@ -81,6 +81,35 @@ function checkTry(rowIndex){
           input.style.color = "white";
        }
       }
+      if(userWord === randomWord.toUpperCase()){
+          popup.classList.remove('hidden');
+        popup.classList.add('flex');
+        popup.innerHTML = `
+         <div class="fixed bg-white z-50 shadow-lg py-5 px-10 md:py-10 md:px-20 rounded-lg flex flex-col items-center justify-center text-base md:text-xl space-y-4">
+      <h1 class="text-gray-700">🎉 You Win!</h1>
+      <h1 class="text-gray-700">  Congratulations! You guessed the word correctly  🚀</h1>
+      <button  id="reset" class="bg-blue-700 text-white py-3 cursor-pointer transform transition-all duration-300  active:scale-95 px-12 rounded-md">Play again</button>
+    </div>
+        `;
+         reset.addEventListener("click",()=>{
+          location.reload();
+         });  
+
+      } else if(rowIndex === 4){
+        popup.classList.remove('hidden');
+        popup.classList.add('flex');
+          popup.innerHTML = `
+         <div class="fixed bg-white z-50 shadow-lg py-5 px-10 md:py-10 md:px-20 rounded-lg flex flex-col items-center justify-center text-base md:text-xl space-y-4">
+      <h1 class="text-gray-700 font-bold">you loose </h1>
+      <h1 class="text-gray-700">The word was <span class="text-blue-800 font-bold">${randomWord}</span></h1>
+      <button id="reset" class="bg-blue-700 text-white py-3 cursor-pointer transform transition-all duration-300  active:scale-95 px-12 rounded-md">Try again</button>
+    </div>
+          `;
+         reset.addEventListener("click",()=>{
+          location.reload();
+         });
+      }
+
 
 }
 for (let i = 0; i < 5; i++) {
@@ -90,6 +119,7 @@ for (let i = 0; i < 5; i++) {
       checkTry(i);
      });
   }
+  
   
 }
 
