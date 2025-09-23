b34ce3d6c2e4480dff2924d52cb35a11acc52792
aymane-3azi <carlejohnsonbus@gmail.com>
2025-09-23 17:12:24 +0100

    new modifucation

--- scripts/app.js
+++ scripts/app.js
@@ -9,7 +9,20 @@ console.log(randomWord);
 
 function generateArea(){
   inputs.innerHTML = "";
-
+  for (let i = 0; i < 5; i++) {
+      inputs.innerHTML += `
+             <div class="tryDiv flex items-center justify-between">
+        </div>
+     `;
+     const tryDiv = document.querySelectorAll(".tryDiv");
+   for (let j = 0; j < randomWord.length; j++) {
+    tryDiv[i].innerHTML += `
+    <input id="letter-${i}-${j}" class="bg-white border-b-4 outline-none text-center  text-2xl border-blue-900 w-10 h-10" type="text" maxlength="1">
+    `; 
+   }
+     
+  }
+/* it
      for(let i = 0 ; i < 5 ; i++){
      inputs.innerHTML += `
              <div id="inputBox${i}" class="flex items-center justify-between">
@@ -22,6 +35,7 @@ function generateArea(){
       `;
      }
     }
+     */
 }
 
 function setAutoFocus(){
@@ -34,11 +48,17 @@ function setAutoFocus(){
   allInputs.forEach((input,index)=>{
     input.addEventListener("input",()=>{
       const nextInput = allInputs[index + 1];
-      if(nextInput && input.value !== ""){
+      if(nextInput && input.value !== "" ){
+///////////////////////////////////
+        if(nextInput === allInputs[randomWord.length]){
+          checkTry();
+        }
+/////////////////////////////////////
         nextInput.focus();
       }
     });
   });
+
    allInputs.forEach((input,index)=>{ 
     input.addEventListener("keydown",(e)=>{
        if(e.key === "Backspace" && input.value === ""){
@@ -53,17 +73,31 @@ function setAutoFocus(){
 
     
 }
-function logic(){
+generateArea();
+
+
+function checkTry(){
+      const divTry = document.querySelectorAll('.tryDiv input');
+  for (let i = 0; i < 5; i++) {
+     for (let j = 0; j < randomWord.length; j++) {
+         if(divTry[i])
+     }
+
+
+      if(divTry[i].value === randomWord[i]){
+        console.log("correct");
+        divTry[i].style.background = "#008236";
+        divTry[i].style.color = "white";
+      }
+}
 
 }
-generateArea();
-btn.addEventListener("click",()=>{
-    for (let i = 0; i < randomWord.length; i++) {
-      
-    }
-});
+
+
+
 setAutoFocus();
 
 
 
-
+//#f0b100 yellow
+//#E7000B red
